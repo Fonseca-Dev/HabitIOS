566798c5fd1cbadb0ff38be26281147e697f7bef
kaue.fonseca <kaue.fonseca@agi.com.br>
2026-05-22 09:17:02 -0300

    Initial Commit

---
+++ HabitIOS/HabitIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitIOSApp.swift
+//  HabitIOS
+//
+//  Created by Kaue Rocha da Fonseca on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
