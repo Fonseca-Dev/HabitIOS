461a4397cb0b7a0e501fa0253b3afca6972178d7
kaue.fonseca <kaue.fonseca@agi.com.br>
2026-05-22 15:59:36 -0300

    Implementacao de NavigationLink e NavigationStack na SignInView

--- HabitIOS/SignIn/View/SignInView.swift
+++ HabitIOS/SignIn/View/SignInView.swift
@@ -10,9 +10,96 @@ import SwiftUI
 struct SignInView: View {
     @ObservedObject var viewModel: SignInViewModel
     
+    @State var email = ""
+    @State var password = ""
+    
+    @State var action: Int? = 0
+    
+    @State var navigationHidden = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .background(.blue)
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20){
+                    Spacer(minLength: 36)
+                    VStack(alignment: .center) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 48)
+                        
+                        Text("Login")
+                            .foregroundColor(.orange)
+                            .font(Font.system(.title).bold())
+                            .padding(.bottom, 8)
+                        
+                        numberField
+                        
+                        passwordField
+                        
+                        enterButton
+                        
+                        registerLink
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(.white)
+            .navigationBarTitle(Text("Login"), displayMode: .inline)
+            .navigationBarHidden(navigationHidden)
+        }
+    }
+}
+
+extension SignInView {
+    var numberField: some View {
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+}
+
+extension SignInView {
+    var passwordField: some View {
+        SecureField("", text: $password)
+            .border(Color.black)
+    }
+}
+
+extension SignInView {
+    var enterButton: some View {
+        Button("Entrar") {
+            
+        }
+    }
+}
+
+extension SignInView {
+    var registerLink: some View {
+        VStack{
+            Text("Ainda não possui um login ativo?")
+                .foregroundColor(.gray)
+                .padding(.top, 48)
+//            let signInViewModel = SignInViewModel()
+            ZStack {
+                NavigationLink(
+                    destination: Text("Tela de Cadastro"),
+                    label: {
+                        Text("Realize seu cadastro")
+                    }
+                )
+//                NavigationLink(
+//                    destination: Text("Tela de Cadastro"),
+//                    tag: 1,
+//                    selection: $action,
+//                    label: {EmptyView()}
+//                )
+//                
+//                Button("Realize seu cadastro") {
+//                    self.action = 1
+//                }
+            }
+        }
     }
 }
 
