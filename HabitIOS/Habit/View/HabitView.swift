0f79941abb39e13e7daaa577658812f920b4d032
kaue.fonseca <kaue.fonseca@agi.com.br>
2026-06-08 15:07:25 -0300

    Elaboracao da HabitView quando estado estiver com Success Empty List

--- HabitIOS/Habit/View/HabitView.swift
+++ HabitIOS/Habit/View/HabitView.swift
@@ -15,9 +15,37 @@ struct HabitView: View {
         ZStack{
             if case HabitUIState.loading = viewModel.uiState {
                 progress
-            } else if case HabitUIState.emptyList = viewModel.uiState {
-            } else if case HabitUIState.fullList = viewModel.uiState {
             } else {
+                NavigationView{
+                    
+                    ScrollView(showsIndicators: false) {
+                        VStack{
+                            
+                            topContainer
+                            
+                            addButton
+                            
+                            if case HabitUIState.emptyList = viewModel.uiState {
+                                
+                                VStack{
+                                    Spacer(minLength: 60)
+                                    
+                                    Image(systemName: "exclamationmark.octagon.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 24, height: 24, alignment: .center)
+                                    
+                                    Text("Nenhum hábito encontrado :(")
+                                }
+                                
+                            } else if case HabitUIState.fullList = viewModel.uiState {
+                                
+                            } else {
+                                
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -26,6 +54,51 @@ struct HabitView: View {
 extension HabitView{
     var progress: some View {
         ProgressView()
+    }
+}
+
+extension HabitView{
+    var topContainer: some View {
+        VStack(alignment: .center, spacing: 12) {
+            Image(systemName: "exclamationmark.triangle")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50, alignment: .center)
+            
+            Text(viewModel.title)
+                .font(Font.system(.title).bold())
+                .foregroundColor(.orange)
+            
+            Text(viewModel.headline)
+                .font(Font.system(.title3).bold())
+                .foregroundColor(Color("textColor"))
+            
+            Text(viewModel.desc)
+                .font(Font.system(.subheadline))
+                .foregroundColor(Color("textColor"))
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 32)
+        .overlay(
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+        .padding(.horizontal, 16)
+        .padding(.top, 16)
+    }
+}
+
+extension HabitView{
+    var addButton: some View {
+        NavigationLink(
+            destination: Text("Tela de adicionar")
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity)
+        ){
+            Label("Criar Hábito", systemImage: "plus.app")
+        }
+        .padding(.horizontal, 16)
     }
 }
 
--- HabitIOS/Habit/ViewModel/HabitViewModel.swift
+++ HabitIOS/Habit/ViewModel/HabitViewModel.swift
@@ -9,5 +9,9 @@ import Combine
 
 class HabitViewModel: ObservableObject {
     
-    @Published var uiState: HabitUIState = .loading
+    @Published var uiState: HabitUIState = .emptyList
+    
+    @Published var title = "Atenção"
+    @Published var headline = " Fique ligado!"
+    @Published var desc = "Você está atrasado nos hábitos"
 }
